a61f6b371a89d3af95e17793d28576f14ddade6d
Andrea Huang <huangandrea@hotmail.it>
2025-04-23 11:44:42 +0200

    8. Refs & Portals :: introducing ref - connecting and accessing HTML elements via refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+// useRef is a hook that returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
+// It is useful for accessing DOM elements directly, without causing a re-render when the value changes.
 
 export default function Player() {
+  const playerName = useRef(); // playerName.current returns the input element
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleNameChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleNameChange} value={enteredPlayerName}/>
+        <input type="text" ref={playerName}/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
